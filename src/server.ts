b7b75e75fff86ee17f0b486b3a7254d595e8f9dc
LAHH19MX <202300028@uthh.edu.mx>
2025-11-02 15:47:05 -0600

    .

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ app.use(
   cors({
     origin:
       process.env.FRONTEND_URL ||
-      "https://frontend-actividad-seguridad-binas.vercel.app/",
+      "https://frontend-actividad-seguridad-binas.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
