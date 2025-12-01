794b300f094372cadee556ef9589c89da6e76068
LAHH19MX <202300028@uthh.edu.mx>
2025-12-01 11:25:03 -0600

    Correccion de URL

--- src/server.ts
+++ src/server.ts
@@ -18,10 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      // "https://frontend-actividad-seguridad-binas.vercel.app",
-    ],
+    origin: ["https://frontend-actividad-seguridad-binas.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-CSRF-Token"],
